383b360b7c45a0a665522b4adcf7fbf28a140075
Andy MacConnell <andrew.macconnell9@googlemail.com>
2023-10-18 21:30:48 +0100

    updating readme
    adding scaffold script

---
+++ scripts/scaffold.ps1
@@ -0,0 +1,29 @@
+mkdir ..\test
+Set-Location ..\test
+
+dotnet new classlib -n Domain -o .\Domain
+
+dotnet new classlib -n Application -o .\Application
+dotnet add .\Application reference .\Domain
+dotnet add .\Application package MediatR
+dotnet add .\Application package Microsoft.Extensions.DependencyInjection.Abstractions
+
+dotnet new classlib -n Infrastructure -o .\Infrastructure
+dotnet add .\Infrastructure reference .\Application
+
+dotnet new classlib -n Persistence -o .\Persistence
+dotnet add .\Persistence reference .\Application
+
+dotnet new webapi -n Api -o .\Api
+dotnet add .\Api reference .\Application
+dotnet add .\Api reference .\Infrastructure
+dotnet add .\Api reference .\Domain
+
+dotnet new sln -n clean-architecture -o .
+dotnet sln . add .\Api
+dotnet sln . add .\Application
+dotnet sln . add .\Domain
+dotnet sln . add .\Infrastructure
+dotnet sln . add .\Persistence
+
+Set-Location ..\scripts
